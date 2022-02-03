a8f340cc9087ecde9620b7a650ee1901c98ad304
Alon Hershenhorn <hershen@gmail.com>
2022-02-03 10:50:40 -0800

    Make code clearer

--- chapter1library.scm
+++ chapter1library.scm
@@ -8,8 +8,7 @@
 (define (sqrt-iter guess x)
         (if (good-enough? guess x)
             guess
-            (sqrt-iter (improve guess x)
-                       x)))
+            (sqrt-iter (improve guess x) x)))
 
 (define (improve guess x)
         (average guess (/ x guess)))
